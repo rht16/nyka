14ec1e47fac7092a9f3e2053623b13aa95ce6971
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2021-12-15 23:05:26 +0530

    homepage added

--- frontend-nyka/src/App.js
+++ frontend-nyka/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {React} from "react";
 import {Home} from "./components/Homepage/Home";
 import {CartPage} from "./components/productsPage/CartPage"
@@ -10,14 +9,6 @@ function App() {
   {/* <CartPage/> */}
     </div>
 
-=======
-
-import {Home} from "./components/Home"
-
-function App() {
-  return (
- <Home />
->>>>>>> bf6c642f14830020a306d51c3a979623f592aac0
   );
 }
 
