1784a557e58b20e8676ef3362b032ae209f58162
rht16 <rohitkrrt@gmail.com>
2021-12-16 14:08:57 +0530

    google auth

--- frontend-nyka/src/App.js
+++ frontend-nyka/src/App.js
@@ -1,9 +1,28 @@
 
 import {Home} from "./components/Home"
-
+import {Payment} from './components/Payment/Payment.jsx'
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/Login/ProctedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+import {Login} from './components/Login/Login'
 function App() {
   return (
- <Home />
+<div>
+<UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/payment"
+                element={
+                  <ProtectedRoute>
+                    <Payment />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              {/* <Route path="/signup" element={<Signup />} /> */}
+            </Routes>
+          </UserAuthContextProvider>
+</div>
   );
 }
 
--- frontend-nyka/src/components/Login/Login_main.jsx
+++ frontend-nyka/src/components/Login/Login_main.jsx
@@ -3,7 +3,27 @@ import './login_main.css'
 import right from '../../Images/right.svg'
 import facebook from '../../Images/facebook.svg'
 import google from '../../Images/google.svg'
+import  { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { Form, Alert } from "react-bootstrap";
+// import { Button } from "react-bootstrap";
+// import GoogleButton from "react-google-button";
+import { useUserAuth } from "../../context/UserAuthContext";
 export const Login_main = ()=>{
+    const [error, setError] = useState("");
+    const { logIn, googleSignIn } = useUserAuth();
+    const navigate = useNavigate();
+    const handleGoogleSignIn = async (e) => {
+      e.preventDefault();
+      try {
+        await googleSignIn();
+        console.log("yes");
+        navigate("/payment");
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
     return (
         <div className="container">
             <div className="first_box12">
@@ -33,7 +53,7 @@ export const Login_main = ()=>{
                        
                     </div>
                     <div className="facebook">
-                        <img src={google} alt="" />
+                        <img src={google} alt=""  onClick={handleGoogleSignIn}/>
                     </div>
                 </div>
                 <p className="number">Or Enter Email-Id Or Phone Number</p>
