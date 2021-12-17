7e54d36af84cec7df360c7513b315ef92d37116a
rht16 <rohitkrrt@gmail.com>
2021-12-17 21:28:19 +0530

    adding

--- frontend-nyka/src/App.js
+++ frontend-nyka/src/App.js
@@ -4,7 +4,6 @@
 //import { CartPage } from "./components/CartPage";
 //import WowSkin from "./components/WowSkin";
 
-<<<<<<< HEAD
 // import {Home} from "./components/Home"
 import {Payment} from './components/Payment/Payment.jsx'
 import { Routes, Route } from "react-router-dom";
@@ -13,25 +12,12 @@ import { UserAuthContextProvider } from "./context/UserAuthContext";
 import {Login} from './components/Login/Login'
 
 // import Checkk from './components/Login/checkk'
-function App() {
-  return (
-<div>
-  {/* <Checkk /> */}
-<UserAuthContextProvider>
-=======
-import { Home } from "./components/Home"
-import { Payment } from './components/Payment/Payment.jsx'
-import { Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./components/Login/ProctedRoute";
-import { UserAuthContextProvider } from "./context/UserAuthContext";
-import { Login } from './components/Login/Login'
-import { CartPage } from "./CartPage"
+
 
 function App() {
   return (
     <div>
       {/* {/* <UserAuthContextProvider>
->>>>>>> 1036a7f658d92861b1b657ee291ce10b1b905532
             <Routes>
               <Route
                 path="/payment"
